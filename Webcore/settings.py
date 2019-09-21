a88a6c6fd3d825b2774fcb8ffe19de7382b399a1
prathamesh patil <prathameshpatil895@gmail.com>
2019-09-21 13:16:45 +0530

    update settings

--- Webcore/settings.py
+++ Webcore/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
@@ -142,7 +143,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
